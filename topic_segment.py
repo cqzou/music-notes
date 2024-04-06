04f3718dfc005238d7da56d183331c23665790fc
carro <cqz@u.northwestern.edu>
2024-04-06 15:15:10 -0500

    format

--- topic_segment.py
+++ topic_segment.py
@@ -10,8 +10,10 @@ load_dotenv()
 
 with open('sample_1.txt', 'r') as file:
 	text = file.read()
+	
+num_topics = 6
 
-prompt = f'Extract 4 to 6 different topics in the following text. Topics must not overlap. For each topic, provide a short title no longer than 4 words. Then, for each topic, write a detailed 2-paragraph encyclopedia-style summary about the topic in simple terms. Format your response in JSON format of "topic": "summary"\n\n{text}'
+prompt = f'Extract {num_topics} different topics in the following text. Topics must not overlap. For each topic, provide a short title no longer than 4 words. Then, for each topic, write a detailed 1-paragraph encyclopedia-style summary about the topic in simple terms. Format your response in JSON format of "topic": " 1-paragraph summary"\n\n{text}'
 
 
 response = client.chat.completions.create(model="gpt-4-turbo-preview",
@@ -20,7 +22,20 @@ messages=[
 ], 
 temperature=0.7,
 max_tokens=3000)
-print(response.choices[0].message.content)
+
+out = response.choices[0].message.content
+
+print(out)
+
+# Remove the Markdown code block syntax (backticks and 'json')
+out_clean = out.strip('`').replace('json\n', '')
+
+# Convert the cleaned JSON string to a Python dictionary
+data = json.loads(out_clean)
+
+# Write the dictionary to a file as JSON
+with open('output.json', 'w') as file:
+	json.dump(data, file, indent=4)
 
 # try:
 # 	topics_dict = json.loads(response.choices[0].message.content[response.choices[0].message.content.find('{'):response.choices[0].message.content.rfind('}')+1])
