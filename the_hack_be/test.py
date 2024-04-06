17d776a6957b525cd8ad3943b0df34b042fb0b61
Shubhanshi-Gaudani <72951422+Shubhanshi-Gaudani@users.noreply.github.com>
2024-04-06 14:58:19 -0500

    suno merged api

--- the_hack_be/test.py
+++
@@ -1,7 +0,0 @@
-import requests 
-
-# For the /uploadfile/ endpoint
-url = "http://127.0.0.1:8000/uploadfile/"
-files = {"file": open("/path/to/your/file.txt", "rb")}
-response = requests.post(url, files=files)
-print(response.json())
