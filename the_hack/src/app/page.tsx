ddf182aecc478ea2e2489200568f733884cb1083
alfenn <52635707+alfenn@users.noreply.github.com>
2024-04-07 00:09:10 -0500

    sad cannot set up db connection on client for mongodb watch()

--- the_hack/src/app/page.tsx
+++ the_hack/src/app/page.tsx
@@ -4,23 +4,54 @@ import { useEffect, useState } from "react";
 import '@fontsource-variable/urbanist';
 import { GetProp, UploadProps } from "antd";
 import { Text, Box, Button, VStack, Container, SimpleGrid, useDisclosure } from "@chakra-ui/react";
-import { Flex } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react';
 import { FileCard } from "@/components/FileCard";
-import { exampleProjects, Project } from "@/components/consts";
+import { ProcessingStatus, UserData, Topic, Project } from "@/components/consts";
 import { FileModal } from "@/components/FileModal";
-import axios from 'axios';
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 
 type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0];
 export default function Home() {
+  const userid: string = 'fioenoe213384fh83833djdiu';
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [fileList, setFileList] = useState<any>([]);
-  const [projectList, setProjectList] = useState<Project[]>(exampleProjects);
+  const [userData, setUserData] = useState<UserData>();
+  // const client = new MongoClient(process.env.MONGO_URI as string);
+  // async function run() {
+  //   try {
+  //     // Connect the client to the server (optional starting in v4.7)
+  //     await client.connect();
+  //     // Send a ping to confirm a successful connection
+  //     await client.db("admin").command({ ping: 1 });
+  //     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  //   } finally {
+  //     // Ensures that the client will close when you finish/error
+  //     await client.close();
+  //   }
+  // }
+  // run().catch(console.dir);
+
+  // const collection = db.collection('inventory');
+  // const changeStream = collection.watch();
+  // changeStream.on('change', next => {
+  //   // process next document
+  // });
+
+  useEffect(() => {
+    fetch(`http://127.0.0.1:8000/getallprojects/${userid}`)
+      .then(response => response.json())
+      .then(json => {
+        setUserData(json);
+        // console.log('fetching', `http://127.0.0.1:8000/getallprojects/${userid}`);
+        // console.log('userData', json);
+      });
+  }, []);
 
   //DEBUG PRINT STATEMENTS, TO BE REMOVED LATER
-  useEffect(() => {
-    console.log(fileList);
-  }, [fileList]);
+  // useEffect(() => {
+  //   console.log(fileList);
+  // }, [fileList]);
 
   const handleUpload = async () => {
   
@@ -79,7 +110,7 @@ export default function Home() {
       </Box>
       <SimpleGrid minChildWidth='300px' spacing='40px' m='10px'>
         {
-          projectList.map((project: Project, index: number) => (
+          userData?.projects.map((project: Project, index: number) => (
             <FileCard key={index} project={project} onClick={onOpen}></FileCard>
           ))
         }
--- the_hack/src/components/consts.tsx
+++ the_hack/src/components/consts.tsx
@@ -1,18 +1,33 @@
+export type ProcessingStatus = "complete" | "processing";
+export interface UserData {
+    userid: string;
+    name: string;
+    email: string;
+    projects: Array<Project>;
+}
+
+export interface Topic {
+    topicname: string;
+    lyrics: string;
+    mp3: string;
+}
 
 export interface Project {
-    processingStatus: string;
+    projectid: string;
+    processingstatus: ProcessingStatus;
+    creationdate: string;
+    projectname: string;
     thumbnail: string;
-    id: string;
-    title: string;
     description: string;
-    creation_date: string;
+    topics: Array<Topic>;
 }
-let example: Project = {
-    "id": "", 
-    "thumbnail": 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1xw:0.74975xh;center,top&resize=1200:*', 
-    "processingStatus": "completed",
-    "title": "Title",
-    "description":"This is a description",
-    "creation_date": "4/6/24"
-}
-export const exampleProjects: Project[] = [example, example, example, example, example]
+
+// let example: Project = {
+//     "id": "", 
+//     "thumbnail": 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1xw:0.74975xh;center,top&resize=1200:*', 
+//     "processingStatus": "completed",
+//     "title": "Title",
+//     "description":"This is a description",
+//     "creation_date": "4/6/24"
+// }
+// export const exampleProjects: Project[] = [example, example, example, example, example]
